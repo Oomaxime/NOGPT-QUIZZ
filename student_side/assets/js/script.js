c8d0d9d88ef6c5ecf337ef0d2a86f96f413b627b
Maxime <maxime.bidan33@gmail.com>
2024-03-04 00:15:35 +0100

    adding switching question and close tab button

--- student_side/assets/js/script.js
+++ student_side/assets/js/script.js
@@ -20,6 +20,7 @@ log_button.addEventListener('click', function(e) {
 });
 
 /**** Toogle full screen when agree rules ****/
+let screen_status= 'OFF'
 const contract_button = document.querySelector('#rules_button');
 contract_button.addEventListener('click', () => {
     document.documentElement.requestFullscreen().catch((e) => {
@@ -27,6 +28,7 @@ contract_button.addEventListener('click', () => {
     });
     rules.style.display = 'none';
     modal.style.display = 'none';
+    screen_status = 'ON'
 });
 
 /****** RULES CHECKER ******/
@@ -48,7 +50,7 @@ function getFullscreenElement() {
         || !!document.msFullscreenElement
 }
 
-setInterval(()=>{if(test_statuts === 'ON'){
+setInterval(()=>{if(screen_status === 'ON'){
     if(getFullscreenElement()=== false){
     /*Récuperer l'info pour mettre 0 a la fin*/
         console.log("mode plein écran désactivez")
@@ -79,6 +81,47 @@ document.addEventListener("keydown", (e) => {
         console.log('mauvaise touche'+ e.key);
     }
 });
+
+/** envois de la réponse et switch vers la question suivante **/
+const qcm_section = document.querySelector('.qcm_section');
+const answer_sending_button = document.querySelector('.answer_sending_button');
+const question_title = document.querySelector('#question_title');
+const main = document.querySelector('main');
+let question_index = 1
+answer_sending_button.addEventListener('click', () => {
+    if (question_index === 1) {
+        question_title.textContent = 'et moi la question 2 ?'
+        qcm_answer_list.innerHTML = '<div>\n'+
+            '<p class="qcm_answer" id="qcm_choice1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula</p>\n'+
+            '<p class="qcm_answer" id="qcm_choice2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula</p>\n'+
+            '</div>\n'+
+            '<div>\n'+
+            '<p class="qcm_answer" id="qcm_choice3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula</p>\n'+
+            '</div>';
+        question_index++;
+    } else if (question_index === 2) {
+        question_title.textContent = 'moi je suis la question 3 ?'
+        qcm_answer_list.innerHTML = '<div>\n'+
+            '<p class="qcm_answer" id="qcm_choice1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula</p>\n'+
+            '<p class="qcm_answer" id="qcm_choice2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula</p>\n'+
+            '</div>\n'+
+            '<div>\n'+
+            '<p class="qcm_answer" id="qcm_choice3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula</p>\n'+
+            '<p class="qcm_answer" id="qcm_choice4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula</p>\n'+
+            '</div>';
+        question_index++;
+    } else if (question_index === 3) {
+        qcm_section.style.display = 'none';
+        main.innerHTML += '<strong id="ending_words">FIN.</strong>\n'+
+            '<button id="close_button">FERMER LA SESSION</button>';
+
+        const close_button = document.querySelector('#close_button');
+        close_button.addEventListener('click', () => {
+            window.close();
+        })
+
+    }
+})
 
 /******* OUTILS DE SÉLECTION DE RÉPONSE QCM ********/
 
@@ -140,3 +183,6 @@ qcm_answer_list.addEventListener('click', (e) => {
             console.log('no valid target')
     }
 })
+
+
+
