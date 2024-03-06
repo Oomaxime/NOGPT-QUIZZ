06efca506dbd6d45d80f56a4d81e75aa63902d37
Max <53225282+Oomaxime@users.noreply.github.com>
2024-03-06 10:11:50 +0100

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const modal = document.querySelector('#login_modal');
-const log_button = document.querySelector('#log_button');
-
-log_button.addEventListener('click', (e) => {
-    modal.style.display = 'none';
-})
